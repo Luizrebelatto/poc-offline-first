79fc404d1e24c0a1ae7fa3c1c1caca637887c3a1
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-05-15 20:43:25 -0300

    pre build expo

--- android/app/src/main/java/com/anonymous/pocofflinefirst/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/pocofflinefirst/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.pocofflinefirst
+package com.anonymous.pocofflinefirst
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/anonymous/pocofflinefirst/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/pocofflinefirst/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.pocofflinefirst
+package com.anonymous.pocofflinefirst
 
 import android.app.Application
 import android.content.res.Configuration
